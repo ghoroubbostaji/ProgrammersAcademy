281189eae6d5471eae8861ead4dbbe523e3a36b2
ghoroubbostaji <ghoroub-bostagi@hotmail.com>
2020-11-25 12:25:10 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 
-const express = require('express')
-var bodyParser = require('body-parser')
+const express = require('express');
+var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-current_path = __dirname
-  
+current_path = __dirname;
+
 app.use(bodyParser.urlencoded({extended : true}) ); //parses data coming from html
 
 
 //-------------------------------
 const mongoose = require('mongoose');
 
-//mongodb+srv://sami:<password>@samicluster.dibvm.mongodb.net/<dbname>?retryWrites=true&w=majority
 mongoose.connect("mongodb://localhost:27017/academydb", {useUnifiedTopology: true, useNewUrlParser: true});
 
 const SessionSchema = new mongoose.Schema (
@@ -34,33 +33,33 @@ const Trainee = mongoose.model("Trainee", TraineeSchema);
 //--------------------------------------------------------
 
 
-app.use(express.static('public'))
-app.use('/css', express.static(current_path + 'public/css'))
-app.use('/js', express.static(current_path + 'public/js'))
-app.use('/images', express.static(current_path + 'public/images'))
-app.use('/fonts', express.static(current_path + 'public/fonts'))
+app.use(express.static('public'));
+app.use('/css', express.static(current_path + 'public/css'));
+app.use('/js', express.static(current_path + 'public/js'));
+app.use('/images', express.static(current_path + 'public/images'));
+app.use('/fonts', express.static(current_path + 'public/fonts'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 
-app.get('/', (req, res) => res.render("pages/index", {title: 'Home'}) )
+app.get('/', (req, res) => res.render("pages/index", {title: 'Home'}) );
 
-app.get('/login', (req, res) => res.render("pages/login", {title: 'Login'}) )
+app.get('/login', (req, res) => res.render("pages/login", {title: 'Login'}) );
 
-app.get('/admin', (req, res) => res.render("pages/admin", {title: 'Admin'}) )
+app.get('/admin', (req, res) => res.render("pages/admin", {title: 'Admin'}) );
 
-app.get('/create_session', (req, res) => res.render("pages/create_session", {title: 'New Session'}) )
-app.get('/reminde', (req, res) => res.render("pages/reminde", {title: 'Reminde'}) )
-app.get('/invite', (req, res) => 
+app.get('/create_session', (req, res) => res.render("pages/create_session", {title: 'New Session'}) );
+app.get('/reminde', (req, res) => res.render("pages/reminde", {title: 'Reminde'}) );
+app.get('/invite', (req, res) =>
 {
   Session.find( (err, mySessions) => {
     res.render('pages/invite', {title: 'Sessions', sessions: mySessions })
   });
-  
+
 });
 
-app.post('/sessions', (req, res) => 
+app.post('/sessions', (req, res) =>
 {
   const s = new Session({
     sname:            req.body.sname,
@@ -70,33 +69,33 @@ app.post('/sessions', (req, res) =>
   });
   s.save();
   res.send(`Success fully addd the Session`)
-})
+});
 
-app.get('/all-sessions', (req, res) => 
+app.get('/all-sessions', (req, res) =>
 {
   Session.find( (err, mySessions) => {
     res.render('pages/all-sessions', {title: 'Sessions', sessions: mySessions })
   });
-  
-})
 
-app.get('/register', (req, res) => 
+});
+
+app.get('/register', (req, res) =>
 {
   Session.find( (err, mySessions) => {
     res.render('pages/register', {title: 'Register'})
   });
-  
-})
 
-app.post('/trainees', (req, res) => 
+});
+
+app.post('/trainees', (req, res) =>
 {
   const s = new Trainee({
     email:            req.body.email,
     pw:       req.body.pw,
   });
   s.save();
-  res.send(`addd the Trainee Successfully`)  
-})
+  res.send(`addd the Trainee Successfully`)
+});
 
 
 
@@ -109,7 +108,7 @@ function send_email(to_email, session_name)
       pass: 'kgsupwbgggkidhof'
     }
   });
-  
+
   msg = `you are invited to ${session_name} please got to our website http://localhost:3000/`
   var mailOptions = {
     from: 'no-reply@gmail.com',
@@ -117,19 +116,19 @@ function send_email(to_email, session_name)
     subject: 'New Session is Availale Welcome 😀',
     text: msg
   };
-  
+
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
       console.log(error);
     } else {
       console.log('Email sent: ' + info.response);
     }
-  });  
+  });
 }
 
-app.get('/invite-session', (req, res) => 
+app.get('/invite-session', (req, res) =>
 {
-  Trainee.find( (err, trainee_list) => 
+  Trainee.find( (err, trainee_list) =>
   {
     for (t of trainee_list)
     {
@@ -138,21 +137,19 @@ app.get('/invite-session', (req, res) =>
     console.log('Emails Done!')
   });
 
-  res.send(`addd the Trainee Successfully`)     
+  res.send(`addd the Trainee Successfully`)
 })
 
-app.get('/invite-session', (req, res) => 
+app.get('/invite-session', (req, res) =>
 {
   session_id = req.query._id
 
-  res.send(`addd the Trainee Successfully`)     
-})
+  res.send(`addd the Trainee Successfully`)
+});
 
 
 //-------------------------------------------------------------------------------
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
-console.log('koko batatis')
-
-
+});
+console.log('koko batatis');
